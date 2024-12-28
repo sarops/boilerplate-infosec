8f9f4c1397746d6ae1dc0845ce29531c68abb31d
AlexSrpls <alexandros.saropoulos@edu.turkuamk.fi>
2024-12-28 17:43:36 +0200

    Updates

--- myApp.js
+++ myApp.js
@@ -2,12 +2,22 @@ const express = require('express');
 const helmet = require('helmet');
 const app = express();
 
+const PORT = process.env.PORT || 3000;
 
-const PORT = process.env.PORT || 3030;
 
-app.listen(PORT, () => {
-  console.log(`Info Security Application Started on Port ${PORT}`);
+module.exports = app;
+const api = require('./server.js');
+app.use(express.static('public'));
+app.disable('strict-transport-security');
+app.use('/api', api);
+app.get("/", function(request, response) {
+  response.sendFile(__dirname + '/views/index.html');
 });
+
+app.listen(port, () => {
+  console.log(`Info Security Application Started on Port ${port}`);
+});
+
 
 
 
